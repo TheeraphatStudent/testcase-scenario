3b7de17cd91a7e7d9af14eb7789e566f6625ca4b
TheeraphatCH <theeraphatchueanokkhum@gmail.com>
2025-05-08 00:24:31 +0700

    feat: hot fix

--- vite.config.ts
+++ vite.config.ts
@@ -10,27 +10,29 @@ import path from 'path';
 //   },
 // });
 
-export default defineConfig({
-  define: {
-    __APP_ENV__: process.env.VITE_VERCEL_ENV,
-  },
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(() => {
+  return {
+    define: {
+      __APP_ENV__: process.env.VITE_VERCEL_ENV,
     },
-  },
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    strictPort: true,
-    host: true,
-    open: true
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+      strictPort: true,
+      host: true,
+      open: true
+    },
+    build: {
+      outDir: 'dist',
+      sourcemap: true,
+    },
+    optimizeDeps: {
+      exclude: ['lucide-react'],
+    },
+  }
 });
